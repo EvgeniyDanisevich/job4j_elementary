e7f5645b92da4436dc7655d6a4bc7b949f341d72
EvgeniyDanisevich <a937va@gmail.com>
2020-12-17 23:27:24 +0300

    4. Расстояние между точками в трехмерном пространстве. [#423812]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -6,12 +6,18 @@ import static java.lang.Math.pow;
 public class Point {
 
     private int x;
-
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public double distance(Point that) {
@@ -21,6 +27,18 @@ public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
         double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return rsl;
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
+    public void info3d() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 
     public static void main(String[] args) {
@@ -38,5 +56,11 @@ public class Point {
         Point d = new Point(6, 7);
         double distSecond = c.distance(d);
         System.out.println(distSecond);
+
+        Point e = new Point(1, 2, 3);
+        Point f = new Point(4, 5, 6);
+        e.info3d();
+        f.info3d();
+        System.out.println(e.distance3d(f));
     }
 }
